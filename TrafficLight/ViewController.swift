e8f759a050237a2a5f3bd18821b6d0182efc1bbd
Sever2686 <sever_3@live.ru>
2024-04-08 19:48:35 +0300

    homework lesson 2.02

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -2,18 +2,58 @@
 //  ViewController.swift
 //  TrafficLight
 //
-//  Created by Алексей Шевченко on 02.04.2024.
+//  Created by Alexey Shevchenko on 02.04.2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+enum lightNow {
+    case red, yellow, green
+}
 
+final class ViewController: UIViewController {
+    @IBOutlet var redLabel: UIView!
+    @IBOutlet var yellowLabel: UIView!
+    @IBOutlet var greenLabel: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var lightNow: lightNow = .red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOf: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 15
+        
+        redLabel.alpha = lightIsOf
+        yellowLabel.alpha = lightIsOf
+        greenLabel.alpha = lightIsOf
+        
+        redLabel.layer.cornerRadius = 50
+        yellowLabel.layer.cornerRadius = 50
+        greenLabel.layer.cornerRadius = 50
     }
 
-
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        switch lightNow {
+        case .red:
+            redLabel.alpha = lightIsOn
+            yellowLabel.alpha = lightIsOf
+            greenLabel.alpha = lightIsOf
+            lightNow = .yellow
+        case .yellow:
+            redLabel.alpha = lightIsOf
+            yellowLabel.alpha = lightIsOn
+            greenLabel.alpha = lightIsOf
+            lightNow = .green
+        case .green:
+            redLabel.alpha = lightIsOf
+            yellowLabel.alpha = lightIsOf
+            greenLabel.alpha = lightIsOn
+            lightNow = .red
+        }
+    }
 }
 
